d527b3d8c7ea9d5071bb9d8f468a8818d3a32f2f
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-08 20:19:22 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,9 @@
-set -ex
-python train.py --dataroot /kaggle/working/font_translator_gan/my_data --model font_translator_gan --name test_new_dataset --no_dropout
+python train.py \
+  --dataroot /kaggle/working/font_translator_gan/my_data \
+  --model font_translator_gan \
+  --name test_new_dataset \
+  --no_dropout \
+  --gpu_ids 0 \
+  --use_wandb \
+  --wandb_project font_translator_gan \
+  --wandb_run_name test_new_dataset
