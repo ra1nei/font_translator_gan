9f589e0b50b8386242a9b5f73fb0c805bcd78f5f
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-08 21:20:49 +0700

    Update font_dataset.py

--- data/font_dataset.py
+++ data/font_dataset.py
@@ -70,7 +70,7 @@ class FontDataset(BaseDataset):
         ### debug
         print(parts)
         ###
-        english_font_path = os.path.join(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], self.style_language)
+        english_font_path = os.path.join(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], self.style_language)
         english_paths = [os.path.join(english_font_path, letter) for letter in random.sample(os.listdir(english_font_path), self.style_channel)]
         return english_paths
     
