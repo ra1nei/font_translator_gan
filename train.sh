d9124747e4cd82140e12431573b30a3d3c661a07
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2025-10-08 19:55:16 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/font --model font_translator_gan --name test_new_dataset --no_dropout
+python train.py --dataroot /kaggle/working/font_translator_gan/my_data --model font_translator_gan --name test_new_dataset --no_dropout
